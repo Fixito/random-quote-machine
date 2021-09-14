60064766f4b7b2e748b6cba5c41c0624a3c9dd9a
Fixito <thomas_3004@hotmail.fr>
2021-09-14 15:30:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import QuoteBox from "./QuoteBox";
 
 function App() {
+  const [color, setColor] = useState("");
+  const colors = [
+    "primary",
+    "secondary",
+    "success",
+    "info",
+    "warning",
+    "danger",
+    "dark",
+  ];
+
+  const getRandomColor = (colors) => {
+    const newColor = colors[Math.floor(Math.random() * colors.length)];
+
+    setColor(newColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main
+      className={`d-flex justify-content-center flex-column align-items-center bg-${color}`}
+    >
+      <QuoteBox color={color} colors={colors} getRandomColor={getRandomColor} />
+    </main>
   );
 }
 
---
+++ src/QuoteBox.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const QuoteBox = ({ color, colors, getRandomColor }) => {
+  const [quote, setQuote] = useState({});
+
+  const getRandomQuote = async () => {
+    const res = await axios.get("https://api.quotable.io/random");
+    const newQuote = res.data;
+    setQuote(newQuote);
+  };
+
+  useEffect(() => {
+    document.title = "FCC: Random Quote Machine";
+
+    getRandomQuote();
+    getRandomColor(colors);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { author, content } = quote;
+
+  return (
+    <>
+      <div
+        id="quote-box"
+        className={`card border-0 p-3 mb-3 text-${color}`}
+        style={{ width: "55%" }}
+      >
+        <div className="card-body">
+          <blockquote className="blockquote mb-0">
+            <p id="author" className="fs-2 mb-4">
+              <i className="fas fa-quote-left"></i> {content}
+            </p>
+            <footer
+              id="text"
+              className={`blockquote-footer  text-${color} float-end`}
+            >
+              {author}
+            </footer>
+          </blockquote>
+        </div>
+        <div className="card-footer border-top-0 d-flex justify-content-between align-items-center">
+          <a href="https://twitter.com/home" id="tweet-quote">
+            <i className={`fab fa-3x fa-twitter-square text-${color}`}></i>
+          </a>
+          <button
+            id="new-quote"
+            className={`btn btn-${color} text-light`}
+            onClick={() => {
+              getRandomQuote();
+              getRandomColor(colors);
+            }}
+          >
+            New quote
+          </button>
+        </div>
+      </div>
+      <p className="text-light" style={{ fontSize: "0.8rem" }}>
+        by Thomas ROBERT
+      </p>
+    </>
+  );
+};
+
+export default QuoteBox;
